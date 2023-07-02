46eae657a3615e6ffd437107e938d26a7ca573d4
Armaan Singh Dhaliwal <117513972+Armaan-Dhaliwal@users.noreply.github.com>
2023-07-02 11:25:16 +0530

    Add files via upload

---
+++ Basic Implementation/BinaryTree_Node_Class.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+template <typename T>
+class BinaryTree{
+    public:
+    T data;
+    BinaryTree<T>* left;
+    BinaryTree<T>* right;
+
+    BinaryTree(T data){
+        this->data=data;
+        left=NULL;
+        right=NULL;
+    }
+
+    ~BinaryTree(){
+        delete left;
+        delete right;
+    }
+
+};
