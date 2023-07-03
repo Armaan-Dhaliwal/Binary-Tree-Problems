2ffc6a1dcb17b557a1873ad16ee5c2faad807fe3
Armaan Singh Dhaliwal <117513972+Armaan-Dhaliwal@users.noreply.github.com>
2023-07-03 13:19:34 +0530

    Add files via upload

---
+++ Basic Implementation/BinaryTree_Level_order_Transversal.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+#include<queue>
+#include"BinaryTree_Node_Class.cpp"
+using namespace std;
+
+BinaryTree<int>* takeInput(){
+    cout<<"enter root data "<<endl;
+    int data;
+    cin>>data;
+    if(data==-1){
+        return NULL;
+    }
+    queue<BinaryTree<int>*> q;
+    BinaryTree<int>* root=new BinaryTree<int>(data);
+     q.push(root);
+
+    while(!q.empty()){
+        cout<<"Enter left data for "<<q.front()->data<<endl;
+        int data;
+        cin>>data;
+        if(data!=-1){
+             BinaryTree<int>* node1 = new BinaryTree<int>(data);
+            q.front()->left=node1;
+            q.push(q.front()->left);
+        }
+
+        cout<<"Enter right data for "<<q.front()->data<<endl;
+        cin>>data;
+        if(data!=-1){
+            BinaryTree<int>* node2 = new BinaryTree<int>(data);
+            q.front()->right=node2;
+            q.push(q.front()->right);
+        }
+        q.pop();
+    }
+    
+    return root;
+}
+
+void PrintTree_Level(BinaryTree<int>* root){
+    if(root==NULL){
+        return ;
+    }
+
+    queue<BinaryTree<int>*> q;
+    BinaryTree<int>* temp = new BinaryTree<int>(-1);
+    q.push(root);
+
+    while(!q.empty()){
+        int nodeCount = q.size();
+        
+        while(nodeCount>0){
+             cout<<q.front()->data<<" ";
+
+        if(q.front()->left!=NULL){
+            q.push(q.front()->left);
+        }
+        
+        if(q.front()->right!=NULL){
+            q.push(q.front()->right);
+        }
+        q.pop();
+        nodeCount--;
+        }
+       cout<<endl;
+    }
+
+}
+
+
+int main(){
+    BinaryTree<int>* root = takeInput();
+    PrintTree_Level(root);
+    delete root;
+}
