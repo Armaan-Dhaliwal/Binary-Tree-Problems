ec364dfae6e8a3c55d852069c905c25d052c9f68
Armaan Singh Dhaliwal <117513972+Armaan-Dhaliwal@users.noreply.github.com>
2023-07-10 22:04:00 +0530

    Add files via upload

---
+++ BinaryTree_Given_Sum.cpp
@@ -0,0 +1,114 @@
+#include<iostream>
+#include<queue>
+#include<algorithm>
+#include"BinaryTree_Node_Class.cpp"
+using namespace std;
+
+BinaryTree<int>* takeInput(){
+    cout<<"enter root data "<<endl;
+    int data;
+    cin>>data;
+    if(data==-1){
+        return NULL;
+    }
+    queue<BinaryTree<int>*> q;
+    BinaryTree<int>* root=new BinaryTree<int>(data);
+     q.push(root);
+
+    while(!q.empty()){
+        cout<<"Enter left data for "<<q.front()->data<<endl;
+        int data;
+        cin>>data;
+        if(data!=-1){
+             BinaryTree<int>* node1 = new BinaryTree<int>(data);
+            q.front()->left=node1;
+            q.push(q.front()->left);
+        }
+
+        cout<<"Enter right data for "<<q.front()->data<<endl;
+        cin>>data;
+        if(data!=-1){
+            BinaryTree<int>* node2 = new BinaryTree<int>(data);
+            q.front()->right=node2;
+            q.push(q.front()->right);
+        }
+        q.pop();
+    }
+    
+    return root;
+}
+
+
+void PrintLevel(BinaryTree<int>* root){
+    queue<BinaryTree<int>*> q;
+    q.push(root);
+    while(!q.empty()){
+        int size = q.size();
+        while(size>0){
+            if(q.front()->left!=NULL){
+                q.push(q.front()->left);
+            }
+            if(q.front()->right!=NULL){
+                q.push(q.front()->right);
+            }
+            cout<<q.front()->data<<" ";
+            q.pop();
+            size--;
+        }
+        cout<<endl;
+    }
+}
+
+vector<int> transverser(BinaryTree<int>* root){
+     vector<int> vec;
+    if(root==NULL){
+        return vec;
+    }
+
+    queue<BinaryTree<int>*> q;
+    q.push(root);
+    while(!q.empty()){
+        if(q.front()->left!=NULL){
+            q.push(q.front()->left);
+        }
+        if(q.front()->right!=NULL){
+            q.push(q.front()->right);
+        }
+        
+        vec.push_back(q.front()->data);
+        q.pop();
+    }
+    return vec;
+}
+
+int main(){
+    BinaryTree<int>* root = takeInput();
+    vector<int> res = transverser(root);
+    sort(res.begin(),res.end());
+    for(int i=0;i<res.size();i++){
+        cout<<res[i]<<" ";
+    }
+    cout<<endl;
+    cout<<"Enter sum: "<<endl;
+    int sum;
+    cin>>sum;
+    cout<<"Pairs are: "<<endl;
+    int i=0;
+    int j=res.size()-1;
+    while(i<j){
+        int tempsum=res[i]+res[j];
+        if(tempsum<sum){
+            i++;
+        }
+        else if(tempsum>sum){
+            j--;
+        }
+        else{
+            //cout<<"temp sum is "<<tempsum<<endl;
+            cout<<res[i]<<" "<<res[j]<<endl;
+            i++;
+            j--;
+        }
+    }
+   
+}
